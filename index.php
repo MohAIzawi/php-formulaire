a40947e9a5029bb5c06f63cc4a370f8494be2c58
Mohamed Al Azawi <alazawimohamed@gmail.com>
2024-05-27 09:04:28 +0200

    update

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+    $name = $email = $subject = $message = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = ($_POST["name"]);
+        $email = ($_POST["email"]);
+        $subject = ($_POST["subject"]);
+        $message = ($_POST["message"]);
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <title>Contactez-moi</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <div class="divider"></div>
+        <div class="heading">
+            <h2>Contactez-moi</h2>
+        </div>
+        <div class="row">
+            <div class="col-lg-10 col-lg-offset-1">
+                <form id="contact-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" role="form">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="name">Nom <span class="blue">*</span></label>
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom" value="<?php echo htmlspecialchars($name); ?>" >
+                        </div>
+                        <div class="col-md-6">
+                            <label for="email">Email <span class="blue">*</span></label>
+                            <input type="email" id="email" name="email" class="form-control" placeholder="Votre email" value="<?php echo htmlspecialchars($email); ?>" >
+                        </div>
+                        <div class="col-md-12">
+                            <label for="subject">Sujet <span class="blue">*</span></label>
+                            <input type="text" id="subject" name="subject" class="form-control" placeholder="Votre sujet" value="<?php echo htmlspecialchars($subject); ?>" >
+                        </div>
+                        <div class="col-md-12">
+                            <label for="message">Message <span class="blue">*</span></label>
+                            <textarea id="message" name="message" class="form-control" placeholder="Votre message" rows="4"><?php echo htmlspecialchars($message); ?></textarea>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="blue"><strong>* Ces informations sont requises</strong></p>
+                        </div>
+                        <div class="col-md-12">
+                            <input type="submit" class="button1" value="Envoyer">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
